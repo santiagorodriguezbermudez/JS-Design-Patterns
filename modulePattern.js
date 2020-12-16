3e0e25f3e1bf4a65fb385cbda7fe0530092f1528
santiagorodriguezbermudez <srba87@gmail.com>
2020-12-16 08:30:48 -0500

    Added the revealing module pattern

--- modulePattern.js
+++ modulePattern.js
@@ -52,3 +52,29 @@ testModule.incrementCounter();
 // Check the counter value and reset
 // Outputs: counter value prior to reset: 1
 testModule.resetCounter();
+
+// Revealing Module Pattern
+var myRevealingModule = (function () {
+    var privateVar = "Ben Cherry",
+        publicVar = "Hey there!";
+
+    function privateFunction() {
+        console.log( "Name:" + privateVar);
+    }
+
+    function publicSetName( strName ) {
+        privateVar = strName;
+    }
+
+    function publicGetName() {
+        privateFunction();
+    }
+
+    // Reveal public pointers to private functions and properties
+
+    return {
+        setName: publicSetName,
+        greeting: publicVar,
+        getName: publicGetName
+    }
+})();
